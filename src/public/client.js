d4e774de0cce06da9160929533e5bfa6fa9dd73c
Guillaume Salicis <gsalicis@d-edge.com>
2024-04-19 16:29:34 +0200

    first

---
+++ src/public/client.js
@@ -0,0 +1,27 @@
+const client = {
+    registrationId: null,
+    logon: () => {
+        // Créer une connexion WebSocket
+        const socket = new WebSocket("ws://localhost:8002");
+
+        // La connexion est ouverte
+        socket.addEventListener("open", function (event) {
+            socket.send("Hello server!");
+        });
+
+        // Écouter les messages
+        socket.addEventListener("message", (event) => {
+            const message = JSON.parse(event.data);
+            console.log("Message from socket:", message);
+            if (message.type === 'registration') {
+                client.registrationId = message.value
+                socket.send("Registration done");
+            }
+        });
+    }
+}
+
+window.gatewayClient = client;
+// console.log('hello');
+// client.logon();
+
