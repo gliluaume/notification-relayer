d4c63bb0e8133695290a104787b254f6fa974d16
gliluaume <salicisguillaume@yahoo.fr>
2024-04-24 23:19:23 +0200

    fixup: add postgres database

--- database/setup.sql
+++ database/setup.sql
@@ -1,38 +1,25 @@
-CREATE DATABASE NotificationRelayer;
-GO
-USE NotificationRelayer
-GO
-
+-- postgres sql version
 CREATE SCHEMA Relayer;
-GO
+CREATE DATABASE NotificationRelayer;
 
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'WebSocketServers') AND type in (N'U'))
-DROP TABLE WebSocketServers;
-GO
+DROP TABLE IF EXISTS WebSocketServers;
 CREATE TABLE WebSocketServers (
-    Id NVARCHAR (30) NOT NULL,
-    [Address] NVARCHAR (200) NOT NULL,
-    ClientId UNIQUEIDENTIFIER CONSTRAINT [DF_WebSocketServers_ClientId] DEFAULT (newid()) ROWGUIDCOL NOT NULL,
-    CONSTRAINT [PK_WebSocketServers_Id] PRIMARY KEY CLUSTERED (Id ASC)
+    Id VARCHAR(30) NOT NULL PRIMARY KEY,
+    Address VARCHAR(200) NOT NULL,
+    ClientId uuid NOT NULL
+    -- ClientId uuid DEFAULT (gen_random_uuid()) NOT NULL
 );
 
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Registrations') AND type in (N'U'))
-DROP TABLE Registrations;
-GO
+DROP TABLE IF EXISTS Registrations;
 CREATE TABLE Registrations (
-    ClientId UNIQUEIDENTIFIER ROWGUIDCOL NOT NULL,
-    ServerId NVARCHAR (30) NOT NULL,
-    CONSTRAINT [PK_Registrations_ClientId] PRIMARY KEY CLUSTERED (ClientId ASC),
-    CONSTRAINT [FK_Registrations_ServerId] FOREIGN KEY (ServerId) REFERENCES WebSocketServers (Id)
+    ClientId UUID NOT NULL PRIMARY KEY,
+    ServerId VARCHAR(30) NOT NULL,
+    CONSTRAINT FK_Registrations_ServerId FOREIGN KEY (ServerId) REFERENCES WebSocketServers (Id)
 );
-GO
 
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'PendingNotifications') AND type in (N'U'))
-DROP TABLE PendingNotifications;
-GO
+DROP TABLE IF EXISTS PendingNotifications;
 CREATE TABLE PendingNotifications (
-    ClientId UNIQUEIDENTIFIER ROWGUIDCOL NOT NULL,
-    [Message] NVARCHAR (500),
-    CONSTRAINT [FK_PendingNotifications_ClientId] FOREIGN KEY (ClientId) REFERENCES Registrations (ClientId)
+    ClientId UUID NOT NULL,
+    Message VARCHAR(500),
+    CONSTRAINT FK_PendingNotifications_ClientId FOREIGN KEY (ClientId) REFERENCES Registrations (ClientId)
 );
-GO
--- src/repository.ts
+++ src/repository.ts
@@ -1,44 +1,23 @@
-import { JSONFilePreset  } from "npm:lowdb@7.0.1/node";
-import { v4 as uuidv4} from "npm:uuid@9.0.1"
+import { Client } from "https://deno.land/x/postgres/mod.ts";
 
-/*
-    WSS table
-    - id: string, the server id, primary key
-    - address: string, the server's url
-
-    REGISTRATIONS table
-    - clientId: UUID, id of the client / registration, primary key
-    - serverId: string, name of the server instance
-
-    PENDING_NOTIFICATIONS table
-    - clientId: UUID, unique
-*/
-
-export type Uuid = uuidv4
-
-export type WebSocketServer = {
-    id: string; // unique
-    address: string;
-}
-
-export type Registrations = {
-    clientId: Uuid; // unique
-    serverId: string;
-}
-
-export type PendingNotifications = {
-    clientId: Uuid; // not unique
-    message?: string;
-}
-
-
-type Data = {
-    webSocketServer: WebSocketServer[];
-    registrations: Registrations[];
-    pendingNotifications: PendingNotifications[];
-}
-
-const defaultData: Data = { messages: [] }
-const db = await JSONFilePreset<Data>('db.json', defaultData)
-
-
+const client = new Client({
+  user: "postgres",
+  database: "notificationrelayer",
+  hostname: "localhost",
+  port: 5432,
+  password: "mysecretpassword",
+});
+await client.connect();
+let res = await client.queryArray(
+  "INSERT INTO WebSocketServers VALUES('Server-001', 'http://localhost:8000', gen_random_uuid())"
+);
+console.log("WebSocketServers", res)
+res = await client.queryArray(
+  "select * from WebSocketServers;"
+);
+console.log("WebSocketServers", res)
+res = await client.queryArray(
+  "DELETE FROM WebSocketServers;"
+);
+console.log("WebSocketServers", res)
+await client.end();
